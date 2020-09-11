b466ee31946ef5c985c645a34767ad2d776912d1
Vladimir Kotal <vlada@devnull.cz>
2020-09-11 16:21:37 +0200

    use lockfile for locking

--- docker/index.sh
+++ docker/index.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-LOCKFILE=/var/run/opengrok-indexer
+#
+# Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+#
+
+LOCK_NAME="index"
 URI="http://localhost:8080"
 # $OPS can be overwritten by environment variable
 OPS=${INDEXER_FLAGS:='-H -P -S -G'}
 
-if [ -f "$LOCKFILE" ]; then
-	date +"%F %T Indexer still locked, skipping indexing"
-	exit 1
-fi
-
-touch $LOCKFILE
+lockfile-create ${LOCK_NAME}
+lockfile-touch ${LOCK_NAME} &
+BADGER="$!"
 
 if [ -z $NOMIRROR ]; then
 	date +"%F %T Mirroring starting"
@@ -30,4 +31,5 @@ opengrok-indexer \
     $INDEXER_OPT "$@"
 date +"%F %T Indexing finished"
 
-rm -f $LOCKFILE
+kill "${BADGER}"
+lockfile-remove ${LOCK_NAME}
